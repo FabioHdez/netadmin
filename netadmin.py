9d810eee8e86f0cd059187c7c7b6fb04b9b5b560
FabioHdez <fahio512@gmail.com>
2025-01-03 18:48:49 -0500

    display menu function

--- netadmin.py
+++ netadmin.py
@@ -1,1 +1,24 @@
-print ('NetAdmin')
+import textwrap
+
+def main():
+    print("")
+
+def display_menu(online_hosts):
+    print(textwrap.dedent(
+        f"""
+        NetAdmin v1.0 \t\t | Online Hosts: {online_hosts}
+        ------------------------------------------------
+        1. Display Hosts
+        2. SSH to Host 
+        3. Ping Host
+        4. Trace Route
+        5. Shutdown / Restart Host
+        6. Edit Settings (IP range, DNS)
+        7. Exit
+        ------------------------------------------------
+        """
+))
+
+if __name__ == "__main__":
+    main()
+    display_menu(10)
