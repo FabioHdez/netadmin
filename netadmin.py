24b292e8da3cbf72b2bb1c71df863de873ce65ce
FabioHdez <fahio512@gmail.com>
2025-01-09 21:49:18 -0500

    cached online hosts and enhanced menu functionality

--- netadmin.py
+++ netadmin.py
@@ -1,6 +1,11 @@
 import nmap
+import sys
 
+#global variables 
+online_hosts = [] #list of online hosts; global because for cach
+sc_width = 0 #screen width
 
+#options for the menu
 menu_options = {
     1: 'Display Hosts',
     2: 'SSH to Host ',
@@ -8,34 +13,49 @@ menu_options = {
     4: 'Trace Route',
     5: 'Shutdown / Restart Host',
     6: 'Edit Settings (IP range, DNS)',
-    7: 'Exit'
+    'q': 'Exit'
 }
 
 def display_menu(online_hosts):
     header = f"\nNetAdmin v1.0 \t\t | Online Hosts: {online_hosts}"
     header = header.expandtabs() #expand tabs to get the correct length
+
+    global sc_width #set the screen width for ui formatting
+    sc_width = len(header)
+
     print (header)
-    print ("-"*len(header))
+    print ("-"*sc_width)
     for option in menu_options:
         print(f"{option}. {menu_options[option]}")
-    print ("-"*len(header))
 
 def scan_network(ip_range):
     print(f"Scanning network {ip_range}...")
-    nm = nmap.PortScanner()
-    nm.scan(hosts=ip_range, arguments='-sn')
-    return nm
+    global online_hosts
+    online_hosts = nmap.PortScanner()
+    online_hosts.scan(hosts=ip_range, arguments='-sn')
 
 def display_hosts():
     #use setting file in the future
-    #use cache in the future
-    #use threading in the future
-    nm = scan_network('10.0.0.0/29') 
-    for host in nm.all_hosts():
-        hostname = nm[host]['hostnames'][0]['name'] if nm[host]['hostnames'] else 'N/A'
-        print(f'IP: {host} - {hostname}')
-    #future display offline hosts too
-    input("Press Enter to continue...")
+    #display saved offline hosts too
+    print ("-"*sc_width)
+    #display cached online hosts
+    def display():
+        for host in online_hosts.all_hosts():
+            hostname = online_hosts[host]['hostnames'][0]['name'] if online_hosts[host]['hostnames'] else 'N/A'
+            print(f'IP: {host} - {hostname}')
+    display()
+    #user input: user can refresh the list, or go back to menu
+    while True:
+        user_input = input("\n1: Refresh hosts\nq: Go back to the main menu\n")
+        if user_input == '1':
+            print ("-"*sc_width)
+            scan_network('10.0.0.0/28') #scan network to refresh the cached online hosts
+            display()
+        elif user_input == 'q':
+            return
+        else:
+            print("Invalid option. Please try again.")
+            continue
     
 def ssh():
     print("ssh to a host")
@@ -58,21 +78,30 @@ def edit():
     input()
 
 def exit_program():
-    exit(0)
+    sys.exit(0)
+
+
 
 if __name__ == "__main__":
+    print("Loading NetAdmin v1.0...")
+    scan_network('10.0.0.0/28') #initial scan
     while (True):
-        display_menu(10)
-        option = input("Select your option: ")
+        display_menu(len(online_hosts.all_hosts()))
+        option = input("\nSelect your option: ").lower()
         option_handle = {
-            1: display_hosts,
-            2: ssh,
-            3: ping,
-            4: traceroute,
-            5: shutdown,
-            6: edit,
-            7: exit_program
+            '1': display_hosts,
+            '2': ssh,
+            '3': ping,
+            '4': traceroute,
+            '5': shutdown,
+            '6': edit,
+            'q': exit_program
         }
-        option_handle[int(option)]()
+        try:
+            option_handle[option]()
+        except:
+            print ("-"*sc_width)
+            print("Please select a valid option.")
+            continue
 
         
