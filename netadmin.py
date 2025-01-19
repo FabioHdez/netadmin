34ccfff661e17ada2f240a911428679ebfce0bc1
FabioHdez <fahio512@gmail.com>
2025-01-19 10:56:14 -0500

    add host to the json config file

--- netadmin.py
+++ netadmin.py
@@ -1,10 +1,14 @@
 import nmap
-import sys, os
+import sys, os, json
+
 
 #global variables 
 online_hosts = [] #list of online hosts; global because for cach
 sc_width = 0 #screen width
 
+#json
+json_file = 'config.json'
+json_data = '' #this is also a cached value
 
 #options for the menu
 menu_options = {
@@ -30,6 +34,11 @@ def display_menu():
     for option in menu_options:
         print(f"{option}. {menu_options[option]}")
 
+def refresh_json():
+    print("Loading configuration file...")
+    global json_data
+    with open(json_file, 'r') as file:
+        json_data = json.load(file)
 
 def scan_network(ip_range):
     print(f"Scanning network {ip_range}...")
@@ -103,14 +112,48 @@ def ping():
     input()
 
 def add():
-    print("traceroute a host")
-    input()
+    display_hosts()
+    #user input: user can refresh the list, or go back to menu
+    while True:
+        user_input = input(f"\n(1-{len(online_hosts.all_hosts())}): Add host \nr: Refresh hosts\nq: Go back to the main menu\n")
+        if user_input == 'r':   ###Refresh
+            print ("-"*sc_width) #Print a line
+            scan_network('10.0.0.0/28') #scan network to refresh the cached online hosts
+            display_hosts() #re-render the new cached hosts
+        elif user_input == 'q':    ###Exit 
+            return
+        elif user_input.isdigit():      ###User selected an option
+            user_input = int(user_input)
+            for index, host in enumerate(online_hosts.all_hosts(),start=1):
+                if user_input == index: #if its a valid option
+                    print ("-"*sc_width)
+                    print(f"Host: {host}")
+                    hostname = input("Enter a hostname: ")
+                    username = input("Enter your username for this host (Leave empty if none): ")
+                    if hostname == '': hostname = 'N/A'
+                    print(f"\nAdded: {host} - {hostname}")
+                    #
+                    try:
+                        new_host = {'ip': host, 'hostname': hostname, 'username':username}
+                        json_data["saved_hosts"].append(new_host)
+                        with open(json_file,'w') as file:
+                            json.dump(json_data, file, indent = 4)
+                        refresh_json()
+                    except Exception as e:
+                        input("There is a problem with the JSON file. You can try to fix it manually (config.json) or restore to default on the 'edit' menu option. \nPress enter to continue...")
+                        return
+                    input("Press enter to continue...")
+                    return
+        else:
+            print("Invalid option. Please try again.")
+            continue
 
 def shutdown():
     print("shutdown a host")
     input()
 
 def edit():
+    #remove, restore to default
     print("edit settings")
     input()
 
@@ -122,6 +165,8 @@ def exit_program():
 if __name__ == "__main__":
     os.system('cls')
     print("Loading NetAdmin v1.0...")
+    refresh_json() #get config data
+    #ip_range = whatever is on the json config file
     scan_network('10.0.0.0/28') #initial scan
     while (True):
         os.system('cls')
