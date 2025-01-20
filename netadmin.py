8befea7321b1b559e12d412601f79f586a5006db
FabioHdez <fahio512@gmail.com>
2025-01-19 21:52:47 -0500

    removed couple of features that can be done using ssh

--- netadmin.py
+++ netadmin.py
@@ -17,9 +17,7 @@ menu_options = {
     2: 'SSH to Host ',
     3: 'Ping Host',
     4: 'Add Host',
-    5: 'Turn on Host',
-    6: 'Restart Host',
-    7: 'Edit Settings (IP range, DNS)',
+    5: 'Edit Settings (IP range, DNS)',
     'q': 'Exit'
 }
 
@@ -169,31 +167,6 @@ def add():
             print("Invalid option. Please try again.")
             continue
 
-def turn_on():
-    print("turn on host")
-    input()
-
-def restart():
-    display_hosts()
-    while True:
-        user_input = input(f"\n(1-{len(online_hosts.all_hosts())}): SSH into host \nr: Refresh hosts\nq: Go back to the main menu\n")
-        if user_input == 'r':
-            print ("-"*sc_width)
-            scan_network(ip_range) #scan network to refresh the cached online hosts
-            display_hosts()
-        elif user_input == 'q':
-            return
-        elif user_input.isdigit():
-            user_input = int(user_input)
-            for index, host in enumerate(online_hosts.all_hosts(),start=1):
-                if user_input == index:
-                    os.system(fr"shutdown -r {host}")
-                    print(f"Restarting - {host}...")
-                    input("Press enter to continue...")
-                    return
-        else:
-            print("Invalid option. Please try again.")
-
 def edit():
     #remove, restore to default
     print("edit settings")
@@ -218,9 +191,7 @@ if __name__ == "__main__":
             '2': ssh,
             '3': ping,
             '4': add,
-            '5': turn_on,
-            '6': restart,
-            '7': edit,
+            '5': edit,
             'q': exit_program
         }
         try:
